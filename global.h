062df9dcc7b58378d5a82cf5116174686a8fa9b2
lafandahalf <Libby@Libbys-MacBook-Air.local>
2013-11-10 19:16:54 -0500

    added comments to clarify token struct

--- global.h
+++ global.h
@@ -1,10 +1,17 @@
-/* This file is holds all the global variables and information needed for the modules in this program to work. */
+/* This file is holds all the global variables and information needed for the modules in this program to work.
+Created by: Kyle
+Date: 11/8/2013
+Last Edit by: Libby
+Edit Date: 11/10/2013 */
 
 
 #define MAXBUFF         256 /* Maximum buffer size for value string */
 #define TYPEBUFF        20  /* Maximum buffer size for type strings */
 
 /* Our token "object" that will be passed around */
+/*@param val: the actual contents of the token (eg. filepath) 
+  @param type: the token type (WORD, STRING, METACHAR, EOL)
+  @param next: pointer to next token in sequence */
 typedef struct{
     char* val;
     char* type;
